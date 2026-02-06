08f21ea98dde8b3c0f798ae64bf91c00ae651513
nithyasree694-bit <nithyasree694@gmail.com>
2026-02-06 06:11:59 +0000

    Delete terraform directory

--- terraform/user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Quick bootstrap; Ansible will finalize and deploy app
-yum update -y
-amazon-linux-extras install -y php8.2
-yum install -y httpd git
-systemctl enable httpd
-systemctl start httpd
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php
