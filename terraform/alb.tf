3c315add5468e939efd27131d23b0f5b3ac4a451
nithyasree694-bit <nithyasree694@gmail.com>
2026-02-06 10:04:19 +0000

    Delete terraform directory

--- terraform/alb.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_lb" "alb" {
-  load_balancer_type = "application"
-  subnets = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
-  security_groups = [aws_security_group.web_sg.id]
-}
--- terraform/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "streamline" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "streamline-vpc" }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.streamline.id
-}
